064ddd4291e4a754436258992dfb2e0cb3c754fa
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-03-24 14:22:31 -0400

    Toggle Improvements
    Added an option to enable debug mode globally and an option to not disable debug mode anywhere.

--- library/ThemeHouse/DisableDebug/Listener/InitDependencies.php
+++ library/ThemeHouse/DisableDebug/Listener/InitDependencies.php
@@ -1,18 +1,23 @@
 <?php
 
-class ThemeHouse_DisableDebug_Listener_InitDependencies extends ThemeHouse_Listener_InitDependencies
+class Waindigo_DisableDebugMode_Listener_InitDependencies extends Waindigo_Listener_InitDependencies
 {
 
     public static function initDependencies(XenForo_Dependencies_Abstract $dependencies, array $data)
     {
         $xenOptions = XenForo_Application::get('options');
-        if ($dependencies instanceof XenForo_Dependencies_Public && $xenOptions->th_disableDebugMode_section != 'admin') {
-            XenForo_Application::setDebugMode(false);
-        } elseif ($dependencies instanceof XenForo_Dependencies_Admin && $xenOptions->th_disableDebugMode_section != 'public') {
-            XenForo_Application::setDebugMode(false);
+        if ($xenOptions->mctrades_enableDebugMode_selection) {
+            XenForo_Application::setDebugMode(true);
+        }
+        if ($xenOptions->waindigo_disableDebugMode_section != 'none') {
+            if ($dependencies instanceof XenForo_Dependencies_Public && $xenOptions->waindigo_disableDebugMode_section != 'admin') {
+                XenForo_Application::setDebugMode(false);
+            } elseif ($dependencies instanceof XenForo_Dependencies_Admin && $xenOptions->waindigo_disableDebugMode_section != 'public') {
+                XenForo_Application::setDebugMode(false);
+            }
         }
 
-        $initDependencies = new ThemeHouse_DisableDebug_Listener_InitDependencies($dependencies, $data);
+        $initDependencies = new Waindigo_DisableDebugMode_Listener_InitDependencies($dependencies, $data);
         $initDependencies->run();
     } /* END initDependencies */
 }
