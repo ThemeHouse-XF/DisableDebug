b3e33be75f3f7c40771f92d17f8daac7f8bbed9f
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-03-24 18:08:50 -0400

    Update Controller.php

--- library/ThemeHouse/DisableDebug/Install/Controller.php
+++ library/ThemeHouse/DisableDebug/Install/Controller.php
@@ -3,5 +3,5 @@
 class ThemeHouse_DisableDebug_Install_Controller extends ThemeHouse_Install
 {
 
-    protected $_resourceManagerUrl = 'https://github.com/MCTradesOrg/DisableDebug';
+    protected $_resourceManagerUrl = 'http://xenforo.com/community/resources/disable-debug-mode.3150/';
 }
